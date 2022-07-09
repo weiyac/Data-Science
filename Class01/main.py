2b1c0988d785e709e5d3911a1def5e5f7809d69b
weiyac <conniechen1619@gmail.com>
2022-07-09 10:39:32 -0700

    Class01.py editing

---
+++ Class01/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+"""a = np.array([1, 2, 3])
+b = np.array([[1, 2, 3], [3.2, 5.8, 6.9]])
+print(a)
+print(a.ndim)
+print(a.shape)
+print(a.dtype)
+print(b)
+print(b.ndim)
+print(b.shape)
+print(b.dtype)"""
+"""a = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(a[1, 2])
+print(a[0, :])
+print(a[:, 0])
+print(a[0,1:5:2])
+a[:, 2] = [10, 12]
+print(a)"""
+"""a = np.array([[[1, 2], [3, 4], [5, 6]],
+              [[-1, -2], [-3, -4], [-5, -6]],
+              [[1, 7], [5, 4], [6, 6]],
+              [[1.3, 2], [3.7, 4], [5.2, 6]],
+              [[1, -2], [3, -4], [5, -6]]])
+print(a.shape)
+print(a[:, 0:2, 1])
+"""
+a = np.zeros((3, 2))
+print(a)
